1a47aaf4cbdf87707f8d5e48762536a9e44c11fe
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2021-06-06 16:11:16 -0300

    feat: app module and main project

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TodoModule } from './app/todo/todo.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mariadb',
+        host: configService.get('DB_HOST', 'localhost'),
+        port: Number(configService.get('DB_PORT', 3306)),
+        username: configService.get('DB_USERNAME', 'root'),
+        password: configService.get('DB_PASSWORD', '123'),
+        database: configService.get('DB_DATABASE', 'todo'),
+        entities: [__dirname + '/**/*.entity{.js,.ts}'],
+        synchronize: true,
+      }),
+    }),
+    TodoModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}

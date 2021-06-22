ca481f35727d22ac379e195e36e50ee538a10ca0
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2021-06-22 19:49:49 -0300

    fix: change http port

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
   SwaggerModule.setup('swagger', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(3333);
 }
 bootstrap();

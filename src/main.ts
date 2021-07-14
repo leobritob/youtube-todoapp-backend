a07bfa6f8c6afbaeb47e7ada97c6b47ac642f6f5
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2021-07-14 10:13:50 -0300

    feat: improve validation pipe

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,9 @@ async function bootstrap() {
 
   SwaggerModule.setup('swagger', app, document);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
   await app.listen(3333);
 }
 bootstrap();

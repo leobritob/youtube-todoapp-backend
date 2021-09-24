d837128fc08dc9c8751350f221b2771fe9989afa
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2021-09-24 10:32:11 -0300

    fix: http port

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
-  await app.listen(3333);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
